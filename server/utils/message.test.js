1fb8255913f7b4c1820deb6cc064fc6522138b40
Nuruzzamancse <mr.nuruzzamancse@gmail.com>
2019-02-27 00:46:47 +0600

    node chat app completed

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -17,10 +17,10 @@ describe('generateMessage', ()=>{
 describe('generateLocationMessage', ()=>{
     it('should generate correct location object', ()=>{
         var from = 'Deb';
-        var latitde = 15;
+        var latitude = 15;
         var longitude = 19;
         var url = 'https://www.google.com/maps?q=15,19';
-        var message = generateLocationMessage(from, latitde, longitude);
+        var message = generateLocationMessage(from, latitude, longitude);
 
         expect(message.createdAt).toBeA('number');
         expect(message).toInclude({from, url});        
